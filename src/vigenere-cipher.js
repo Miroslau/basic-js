3f8e44f9cd773417a27028f8ba5ba18285764fa9
Miroslau <mira2408@mail.ru>
2020-09-15 19:49:01 +0300

    feat: solved the vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  encrypt(msg, key) {
+    let result = "";
+    let keyIndex = 0;
+ 
+    for(let symbol of msg){
+        let indexLetter = (this.alphabet.indexOf(symbol) + this.alphabet.indexOf(key[keyIndex])) % this.alphabet.length;
+        result = result + this.alphabet[indexLetter];
+        keyIndex++;
+        if(keyIndex === key.length){
+            keyIndex = 0;
+        }
+    }
+ 
+    return result;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(msg, key) {
+    let result = "";
+    let keyIndex = 0;
+    
+    for(let symbol of msg){
+      let indexLetter = (this.alphabet.indexOf(symbol) + this.alphabet.length - this.alphabet.indexOf(key[keyIndex])) % this.alphabet.length;
+      result += this.alphabet[indexLetter];
+      keyIndex++;
+      if(keyIndex === key.length){
+        keyIndex = 0;
+      }
+    }
+  return result;
   }
 }
 
